a978edadab9820147c845c1a2b00a45e8ab1ad89
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-16 19:02:04 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import ListBeers from './components/ListBeers';
 export default function App() {
+  const [beers, setBeers] = useState();
+  useEffect(() => {
+    const api = 'https://api.punkapi.com/v2/beers?page=2&per_page=8';
+    fetch(api)
+      .then((res) => res.json())
+      .then((response) => {
+        // console.log(response);
+        setBeers(response);
+      })
+      .catch((error) => {
+        console.log('failed to load data');
+      });
+  }, [beers]);
   return (
     <div>
-      <ListBeers />
+      <ListBeers beers={beers} />
     </div>
   );
 }
--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
-export default function ListBeers() {
-  const [beers, setBeers] = useState();
-  let curr_page = 0,
-    prev_page = 0,
-    next_page = 0;
-  useEffect(() => {
-    const api = 'https://api.punkapi.com/v2/beers?page=2&per_page=8';
+export default function ListBeers({ beers }) {
+  // const [beers, setBeers] = useState();
+  const [curr_page, setCurr] = useState(1);
+  const [prev_page, setPrev] = useState();
+  const [next_page, setNext] = useState();
+  
+  // let curr_page = 0,
+  //   prev_page = 0,
+  //   next_page = 0;
+  // useEffect(() => {
+  //   const api = 'https://api.punkapi.com/v2/beers?page=2&per_page=8';
+  //   fetch(api)
+  //     .then((res) => res.json())
+  //     .then((response) => {
+  //       // console.log(response);
+  //       setBeers(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log('failed to load data');
+  //     });
+  // }, [beers]);
+  const page_step = 80;
+
+  const nextPage = ({ curr }) => {
+    const api = `https://api.punkapi.com/v2/beers?page=2&per_page=${
+      curr + page_step
+    }`;
     fetch(api)
       .then((res) => res.json())
       .then((response) => {
@@ -15,18 +35,24 @@ export default function ListBeers() {
       .catch((error) => {
         console.log('failed to load data');
       });
-  }, [beers]);
+  };
 
   return (
     <div>
       <h1>List of Beers</h1>
-      {beers &&
-        beers.map((item) => {
-          return <div key={item.id}>{item.name}</div>;
-        })}
-      <button>Prev</button>
-      Page: {curr_page}
-      <button>Next</button>
+      {beers && beers.length > 0 ? (
+        <>
+          {beers &&
+            beers.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          <button>Prev</button>
+          Page: {curr_page}
+          <button onClick={nextPage}>Next</button>
+        </>
+      ) : (
+        <h4>Loading...</h4>
+      )}
     </div>
   );
 }
