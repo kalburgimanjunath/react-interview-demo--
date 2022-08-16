dee69ba01fb352e51615e0271f1ad0cc8b360ab2
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-16 20:54:11 +0530

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 import ListBeers from './components/ListBeers';
+// import beers from './data/data.js';
 export default function App() {
-  const [beers, setBeers] = useState();
+  const [beers, setBeers] = useState([]);
+  const [curr_page, setCurr] = useState(1);
+  const [prev_page, setPrev] = useState(0);
+  const [next_page, setNext] = useState(0);
   useEffect(() => {
-    const api = 'https://api.punkapi.com/v2/beers?page=2&per_page=8';
+    const page_step = 8;
+    const api = `https://api.punkapi.com/v2/beers?page=${curr_page}&per_page=${page_step}`;
+    fetch(api)
+      .then((res) => res.json())
+      .then((response) => {
+        setBeers(response);
+      })
+      .catch((error) => {
+        console.log('failed to load data');
+      });
+  }, [curr_page]);
+  const updateBeer = () => {
+    console.log(curr_page);
+    const page_step = 8;
+    const api = `https://api.punkapi.com/v2/beers?page=${curr_page}&per_page=${page_step}`;
     fetch(api)
       .then((res) => res.json())
       .then((response) => {
@@ -14,9 +32,25 @@ export default function App() {
       .catch((error) => {
         console.log('failed to load data');
       });
-  }, [beers]);
+  };
+
+  const nextPage = () => {
+    updateBeer();
+    setCurr(curr_page + 1);
+    setPrev(curr_page);
+  };
+  const prevPage = () => {
+    updateBeer();
+    setCurr(curr_page - 1);
+    setPrev(curr_page);
+  };
   return (
-    <div>
+    <div className="main">
+      <>
+        <button onClick={prevPage}>Prev</button>
+        <h3>Page: {curr_page}</h3>
+        <button onClick={nextPage}>Next</button>
+      </>
       <ListBeers beers={beers} />
     </div>
   );
--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -1,54 +1,24 @@
 import React, { useState, useEffect } from 'react';
 export default function ListBeers({ beers }) {
   // const [beers, setBeers] = useState();
-  const [curr_page, setCurr] = useState(1);
-  const [prev_page, setPrev] = useState();
-  const [next_page, setNext] = useState();
-  
-  // let curr_page = 0,
-  //   prev_page = 0,
-  //   next_page = 0;
-  // useEffect(() => {
-  //   const api = 'https://api.punkapi.com/v2/beers?page=2&per_page=8';
-  //   fetch(api)
-  //     .then((res) => res.json())
-  //     .then((response) => {
-  //       // console.log(response);
-  //       setBeers(response);
-  //     })
-  //     .catch((error) => {
-  //       console.log('failed to load data');
-  //     });
-  // }, [beers]);
-  const page_step = 80;
-
-  const nextPage = ({ curr }) => {
-    const api = `https://api.punkapi.com/v2/beers?page=2&per_page=${
-      curr + page_step
-    }`;
-    fetch(api)
-      .then((res) => res.json())
-      .then((response) => {
-        // console.log(response);
-        setBeers(response);
-      })
-      .catch((error) => {
-        console.log('failed to load data');
-      });
-  };
 
   return (
-    <div>
-      <h1>List of Beers</h1>
+    <div className="card_container">
       {beers && beers.length > 0 ? (
         <>
           {beers &&
             beers.map((item) => {
-              return <div key={item.id}>{item.name}</div>;
+              return (
+                <div key={item.id} className="card" id={item.id}>
+                  <h1>{item.name}</h1>
+                  <img src={item.image_url} width="100" height="100" />
+                  <div>{item.tagline}</div>
+                  <div className="disc">
+                    <i>{item.description}</i>
+                  </div>
+                </div>
+              );
             })}
-          <button>Prev</button>
-          Page: {curr_page}
-          <button onClick={nextPage}>Next</button>
         </>
       ) : (
         <h4>Loading...</h4>
